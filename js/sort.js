49b7b4196254674ae5911c2d24f5ce1290186829
Ksenia <demidova.kd@gmail.com>
2018-08-13 12:49:04 +0300

    add function to add rows in tab'

--- js/sort.js
+++ js/sort.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-
+	var arr_f=[];
+	function getArrFromTab() {
 		var tab=document.getElementById('tab1');
 		var arr=[];
 		var arr_tr=[];
@@ -11,6 +12,23 @@ $(document).ready(function() {
 			arr.push(arr_tr);
 			arr_tr=[];
 		}
+	}
+	setInterval(function(){
+		if(document.location.href=='http://table/') {
+
+		//function getArrFromTab() {
+		var tab=document.getElementById('tab1');
+		var arr=[];
+		var arr_tr=[];
+		for (var i=1; i<tab.rows.length; i++) {
+			for (var j=0; j<tab.rows[i].cells.length; j++) {
+				arr_tr.push(tab.rows[i].cells[j].innerHTML);
+				
+			}
+			arr.push(arr_tr);
+			arr_tr=[];
+		}
+	//}	
 
 	function sortYear() {
 		arr.sort(function(a,b){
@@ -68,6 +86,36 @@ $(document).ready(function() {
 	$('#year').click(function(){sortYear(); changeTab()});	
 	$('#rating').click(function(){sortRate(); changeTab()});	
 	$('#min').click(function(){sortMin(); changeTab()});
-	$('#country').click(function(){sortCountry(); changeTab()});	
+	$('#country').click(function(){sortCountry(); changeTab()});
 
+
+	}
+	
+	}, 2000)	
+	
+
+	$('#add_film').click(function(){
+		
+			function addElementsFromForm(a){
+				arr_f.push(document.getElementById(a).value);
+				return arr_f;
+			}
+
+			addElementsFromForm('name_f');
+			addElementsFromForm('year_f');
+			addElementsFromForm('rate_f');
+			addElementsFromForm('length_f');
+			addElementsFromForm('country_f');
+
+			var new_html=window.opener.document.createElement('tr');
+			new_html.innerHTML = "<td>"+arr_f[0]+"</td><td>"+arr_f[1]+"</td><td>"+arr_f[2]+"</td><td>"+arr_f[3]+"</td><td>"+arr_f[4]+"</td>";
+		
+			window.opener.document.getElementById('tb').appendChild(new_html);
+			window.close();		
+	});	
+
+	
+	
+
+	
 });
